7a23a2533594aeda867cd3581c7ed7e5ba3487b3
Johan Kämpe <kampe.johan@gmail.com>
2018-03-01 21:00:57 +0100

    config: Configuration manager using settings.ini

---
+++ config.py
@@ -0,0 +1,33 @@
+# Handle settings in settings.ini
+
+import paths
+import configparser, os
+from printout import print_warning
+
+class configuration_manager:
+    def __init__(self):
+        self.config = None;
+        self.filename = os.path.dirname(os.path.realpath(__file__)) + "\settings.ini"
+        self.load_success = False
+        self._load_settings()
+
+    def _load_settings(self):
+        if os.path.isfile(self.filename):
+            try:
+                self.config = configparser.ConfigParser()
+                self.config.read(self.filename)
+                self.load_success = True
+            except:
+                print_warning("configuration_manager: Could not load {}".
+                    format(self.filename))
+        else:
+            print_warning("configuration_manager: file missing: {}".
+                format(self.filename))
+
+    def get_setting(self, section, key):
+        if section in self.config and key in self.config[section]:
+            return self.config[section][key]
+        else:
+            print_warning("configuration_manager: {}:{} does not exist in {}".
+                format(section, key, self.filename))
+            return None
