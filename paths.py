e177a7a69dd5144cdcd4cc35ecf7d12b6e344004
Johan Kämpe <kampe.johan@gmail.com>
2018-03-01 19:39:43 +0100

    Use settings.ini to store mod paths

--- paths.py
+++ paths.py
@@ -1,7 +1,8 @@
 import sys, os
 
-home = os.getenv("HOME")
+import configparser
+config = configparser.ConfigParser()
+config.read('settings.ini')
 
-for mod_path in ["db" ,"io", "omdb-search", "file"]:
-    p = os.path.join(home, "code", mod_path)
-    sys.path.append(p)
+for mod_path in config['modpath']:
+    sys.path.append(config['modpath'][mod_path])
