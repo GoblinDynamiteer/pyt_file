6b141e59fe251f8b8226a897b7fe9b5987057b5a
Johan Kämpe <kampe.johan@gmail.com>
2018-03-18 13:33:15 +0100

    filetools: add script name to printouts, add func: copy_dbs_to_webserver

--- filetools.py
+++ filetools.py
@@ -1,8 +1,26 @@
 import platform, os, re
 from datetime import datetime
 import diskstation as ds
-from printout import print_warning
 from shutil import copy2
+from printout import print_blue, print_no_line, print_color_between
+from printout import print_script_name as psn
+from printout import print_color_between as pcb
+from printout import print_error
+from printout import print_warning
+from config import configuration_manager as cfg
+
+_config = cfg()
+
+# Print with script name as prefix
+def print_log(string, category=None):
+    script = os.path.basename(__file__)
+    psn(script, "", endl=False) # Print script name
+    if category == "error":
+        print_error("Error: ", endl=False)
+    if string.find('[') >= 0 and string.find(']') > 0:
+        pcb(string, "blue")
+    else:
+        print(string)
 
 # Try to determine creation date of folder
 def get_creation_date(path_to_file_or_folder, convert=False):
@@ -11,7 +29,8 @@ def get_creation_date(path_to_file_or_folder, convert=False):
     if platform.system() == 'Windows':
         ret_time = os.path.getctime(path_to_file_or_folder)
         ret_time.replace(microsecond=0)
-        return ret_time if convert is False else datetime.fromtimestamp(ret_time)
+        return ret_time if convert is False \
+            else datetime.fromtimestamp(ret_time)
 
 # Create nfo file with IMDb-id for movie
 def create_nfo(full_path, imdb):
@@ -22,18 +41,22 @@ def create_nfo(full_path, imdb):
                 newfile.write(imdb)
             return True
         except:
-            print_warning("create_nfo: Could not create movie.nfo: {}".format(full_path))
+            print_log("create_nfo: could not create movie.nfo: {}"
+                .format(full_path), category="warning")
             return False
     else:
-        print_warning("create_nfo: movie.nfo already exists: {}".format(full_path))
+        print_log("create_nfo: movie.nfo already exists: {}".format(full_path),
+            category="warning")
         return True
 
+# Check if file is empty
 def is_file_empty(full_path):
     try:
         if os.stat(full_path).st_size is 0:
             return True
     except:
-        print_warning("is_file_empty: could not check file {}".format(full_path))
+        print_log("is_file_empty: could not check file {}".format(full_path),
+            category="warning")
         return False
 
 # Copy file to dest, append  YYYY-MM-DD-HHMM
@@ -46,10 +69,20 @@ def backup_file(src_full_path, dest_dir_full_path):
         copy2(src_full_path, dest)
         return True
     except:
-        print_warning("backup_file: Could not backup file: {}".format(src_full_path))
-        print("Make sure to run scripts as sudo!")
+        print_log("backup_file: could not backup file: {}"
+            .format(src_full_path), category="warning")
+        print_log("backup_file: make sure to run scripts as sudo!",
+            category="warning")
         return False
 
+# Copy databases to webserver file loc
+def copy_dbs_to_webserver():
+    htdoc_loc = _config.get_setting("path", "webserver")
+    movdb_loc = _config.get_setting("path", "movdb")
+    copy2(movdb_loc, htdoc_loc)
+    print_log("copied to webserver htdocs: {}".format(movdb_loc));
+
+# Helper function to guess_folder_type
 def _type_points(folder):
     regex = {'season': '\.[Ss]\d{2}\.', 'episode': "\.[Ss]\d{2}[Ee]\d{2}",
              'movie': "\.\d{4}\.\d{3,4}p\."}
@@ -59,6 +92,7 @@ def _type_points(folder):
             points[key] += 1
     return points
 
+# Helper function to guess_folder_type
 def _is_regex_match(regex, string):
     rgx = re.compile(regex)
     match = re.search(rgx, string)
