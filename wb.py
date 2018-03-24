4f72273d82463a03e29e7f99661910d67b16fbb4
Johan Kämpe <kampe.johan@gmail.com>
2018-03-24 18:30:39 +0100

    wb: add arguments for filtering wbnew list
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- wb.py
+++ wb.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-import subprocess, shlex, os, paths, filetools
+import subprocess, shlex, os, paths, filetools, argparse, re
 from printout import print_script_name as psn
 from printout import print_color_between as pcb
 from printout import print_no_line as pnl
 from printout import print_error
 from datetime import datetime
 import db_mov as movie_database
-
-db = movie_database.database()
 
 def print_log(string, category=None):
     script = os.path.basename(__file__)
@@ -25,13 +23,30 @@ class file_list:
         self.raw_data = raw_data #Raw data from subprocess out
         self.raw_lines = self._raw_data_to_lines(raw_data)
         self.line_data = [] #empty list
+        counter = 1
         for line in self.raw_lines:
             d = self._line_to_dict(line)
             if d:
+                d["count"] = counter
+                counter += 1
                 self.line_data.append(d)
 
-    def _print_list(self):
+    def _print_list(self, list_type, name_filter, nodb):
         for line in self.line_data:
+            if line['guessed_type']:
+                type = line['guessed_type'][0]
+            else:
+                type = "-"
+            name = line['name']
+            if name_filter:
+                na = re.compile(name_filter)
+                result = na.search(name)
+                if result is None:
+                    continue
+            if list_type and list_type != type:
+                continue
+            if nodb and line['in_db']:
+                continue
             dtstr = line['date'].strftime("%Y-%m-%d %H:%M:%S")
             print_log("")   # print script name
             pnl(dtstr)      # print date string
@@ -40,10 +55,11 @@ class file_list:
             else:
                 pcb(" [nodb]", "yellow", endl=False)
             if line['guessed_type']:
-                pcb("[{}]".format(line['guessed_type'][0]), "green", endl=False)
+                pcb("[{}]".format(type), "green", endl=False)
             else:
                 pcb("[{}]".format("-"), "red", endl=False)
-            pcb("[ {} ]".format(line['name']), "blue")
+            pnl(" {0:0>3} ".format(line['count']))
+            pcb("[ {} ]".format(name), "blue")
 
 
     def _raw_data_to_lines(self, raw_data):
@@ -69,8 +85,8 @@ class ls_command:
         self.output = self._run()
         self.files = file_list(self.output)
 
-    def print_files(self):
-        self.files._print_list()
+    def print_files(self, list_type=None, name_filter=None, nodb=False):
+        self.files._print_list(list_type, name_filter, nodb)
 
     def _generate_command(self):
         lsc = "ls -trl --time-style=\"+%Y-%m-%d %H:%M\" {}".format(self.dir)
@@ -95,6 +111,25 @@ class ls_command:
         print_log("command: [ {} ]".format(self.command))
         print_log("args: [ {} ]".format(self.args))
 
-def wbnew():
+def wbnew(args):
     lsc = ls_command("~/files", remote="wb")
-    lsc.print_files()
+    lsc.print_files(list_type=args.type, name_filter=args.filter,
+        nodb=args.nodb)
+
+db = movie_database.database()
+parser = argparse.ArgumentParser(description='WBTools')
+parser.add_argument('func', type=str, help='WB command: new, get')
+parser.add_argument('-t', '--type', dest='type', default=None,
+    help='filter new list by types: m, t, s')
+parser.add_argument('-f', '--filter', dest='filter', default=None,
+    help='filter new list by string')
+parser.add_argument('-nodb', dest='nodb', action="count", default=False,
+    help='Display only items not in db')
+args = parser.parse_args()
+
+if(args.func == "new"):
+    wbnew(args)
+elif(args.func == "get"):
+    print("Will run get!")
+else:
+    print_log("Wrong command: {}".format(args.func), category="error")
