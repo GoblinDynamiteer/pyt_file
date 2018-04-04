5488c730b20eda1de8c302b9756e9580529e454d
Johan Kämpe <kampe.johan@gmail.com>
2018-04-04 14:26:41 +0200

    extract: handle episodes unrar/move

--- extract.py
+++ extract.py
@@ -6,9 +6,9 @@ from printout import print_class as pr
 
 pr = pr(os.path.basename(__file__))
 _config = cfg()
+script = os.path.basename(__file__)
 
 def check_valid_source_folder(source_path):
-    script = os.path.basename(__file__)
     if not os.path.exists(source_path): # Input folder is not a real dir
         pr.error("[ {} ] does not exist, quitting!".format(source_path))
         exit()
@@ -28,6 +28,37 @@ def move_mov(file):
         os.system("mv {} {}".format(source_path, dest_path))
         pr.info("File moved!")
 
+# Move episode
+def move_ep(file_name_s):
+    src = os.path.join(cwd, file_name_s)
+    dest = tvshow.create_new_show_from_ep(file_name_s)
+    pr.info(f'move [{file_name_s}]')
+    pr.info(f'---> [{dest}]')
+    if user_input.yes_no("Proceed with move?", script_name=None):
+        # TODO: Use subprocess.call instead of os.system
+        os.system("mv {} \"{}\"".format(src, dest))
+        pr.info("File moved!")
+
+# Move episode
+def extract_ep(folder):
+    src = os.path.join(cwd, folder)
+    dest = tvshow.create_new_show_from_ep(folder)
+    rar_file = None
+    for f in os.listdir(src):
+        if f.endswith(".rar"):
+            rar_file = str(f)
+    if rar_file is None:
+        pr.warning(f"could not find .rar in [{folder}]")
+        quit()
+    src_rar = os.path.join(src, rar_file)
+    pr.info(f"found rar-file: [{rar_file}]")
+    pr.info(f'extract [{folder}]')
+    pr.info(f'------> [{dest}]')
+    if user_input.yes_no("proceed with extraction?", script_name=None):
+        # TODO: Use subprocess.call instead of os.system
+        os.system("unrar e \"{}\" \"{}\"".format(src_rar, dest))
+        pr.info("done!")
+
 # Find rar and nfo-files passed in directory
 def extract_mov(folder):
     source_path = os.path.join(cwd, folder)
@@ -42,7 +73,7 @@ def extract_mov(folder):
             nfo_file = str(f)
     # TODO: Check != sub rar
     if rar_file is None:
-        print("Could not find .rar in {}". format(folder))
+        pr.warning(f"could not find .rar in [{folder}]")
         quit()
     source_file = os.path.join(source_path, rar_file)
     pr.info("Found rar-file: [ {} ]".format(os.path.basename(source_file)))
@@ -103,18 +134,23 @@ parser.add_argument('dir', type=str, help='Path to movie or tv source')
 args = parser.parse_args()
 
 dir_name = movie.remove_extras_from_folder(args.dir)
-print(dir_name)
 cwd = os.getcwd() # Get working directory
 guessed_type = filetools.guess_folder_type(dir_name)
 
 if guessed_type == 'movie':
     pr.info("Guessed movie!")
     if(args.dir.endswith(".mkv")):
+        pr.info("is file (mkv)")
         move_mov(args.dir)
     else:
         extract_mov(args.dir)
 elif guessed_type == 'episode':
     pr.info("Guessed tv episode!")
+    if(args.dir.endswith(".mkv")):
+        pr.info("is file (mkv)")
+        move_ep(args.dir)
+    else:
+        extract_ep(args.dir)
 elif guessed_type == 'season':
     pr.info("Guessed tv season!")
     extract_season(args.dir)
