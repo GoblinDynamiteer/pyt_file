88c5211811da5baf219faeccbb6816c6243736e1
Johan Kämpe <kampe.johan@gmail.com>
2018-04-28 19:32:58 +0200

    diskstation: handle restore share

--- diskstation.py
+++ diskstation.py
@@ -8,13 +8,21 @@ pr = pr(os.path.basename(__file__))
 _config = cfg()
 
 def get_mount_points():
-    return ['TV', 'FILM', 'MISC', 'BACKUP', 'DATA']
+    return ['TV', 'FILM', 'MISC', 'BACKUP', 'DATA', 'Rest']
 
 def get_mount_dest():
     return _config.get_setting("path", "dsmount")
 
 def get_home():
     return _config.get_setting("path", "home")
+
+def _get_credentials_file(share):
+    if share == 'Rest':
+        return get_home() + ".smbcredentials_drb"
+    return get_home() + ".smbcredentials"
+
+def _print_error_invalid_share(string):
+    pr.error(f"invalid share: [{string}]")
 
 #Mount share
 def mount(ds_share):
@@ -23,26 +31,28 @@ def mount(ds_share):
         quit()
     dsip="192.168.0.101"
     mount_dest = get_mount_dest()
-    home = get_home()
-    opt = "credentials={}.smbcredentials,iocharset=utf8,vers=3.0,rw,file_mode=0777,dir_mode=0777".format(home)
     ds_shares = get_mount_points()
     if ds_share == "all" or ds_share.upper() in ds_shares:
         for share in ds_shares:
             if share == ds_share.upper() or ds_share == "all":
                 if ismounted(share):
-                    pr.info("{} is mounted at {}".format(share, get_mount_path(share)))
+                    pr.info(f"{share} is mounted at {get_mount_path(share)}")
                     continue
-                src = "//{}/{}".format(dsip, share)
-                local_dest = "{}{}".format(mount_dest, share.lower())
-                command = "sudo mount -t cifs {} {} -o {}".format(src, local_dest, opt)
-                pr.info("Mounting {} to {}".format(share, local_dest))
+                cred_file = _get_credentials_file(share)
+                opt = f"credentials={cred_file},iocharset=utf8,vers=3.0,rw,file_mode=0777,dir_mode=0777"
+                src = f"//{dsip}/{share}"
+                local_dest = f"{mount_dest}{share.lower()}"
+                command = f"sudo mount -t cifs {src} {local_dest} -o {opt}"
+                pr.info(f"mounting {share} to {local_dest}")
                 #os.system(command)
                 subprocess.call(["sudo", "mount", "-t", "cifs", src, local_dest, "-o", opt])
     else:
-        pr.error("Invalid share: [{}]".format(ds_share))
+        _print_error_invalid_share(ds_share);
 
 # Check if share is already mounted
 def ismounted(ds_share):
+    if ds_share.lower() == 'rest':
+        return
     ds_shares = get_mount_points()
     mount_dest = get_mount_dest()
     if ds_share.upper() in ds_shares :
@@ -52,29 +62,32 @@ def ismounted(ds_share):
             return True
         return False
     else:
-        pr.error("Invalid share: [{}]".format(ds_share))
+        _print_error_invalid_share(ds_share);
 
 # Get local mount path of ds share
 def get_mount_path(ds_share):
+    if ds_share.lower() == 'rest':
+        return
     ds_shares = get_mount_points()
     mount_dest = get_mount_dest()
     if ds_share.upper() in ds_shares :
         local_dest = os.path.join(mount_dest, ds_share.lower())
         return local_dest
     else:
-        pr.error("Invalid share: [{}]".format(ds_share))
+        _print_error_invalid_share(ds_share);
         return None
 
 # Print to console ifmounted
 def print_ifmounted(ds_share):
     ds_shares = get_mount_points()
     for share in ds_shares:
+        if share == 'Rest':
+            continue
         if ds_share == "all" or ds_share.lower() == share:
             if ismounted(share):
-                pr.info("{} is mounted at {}".format(share,
-                    get_mount_path(share)))
+                pr.info(f"{share} is mounted at {get_mount_path(share)}")
             else:
-                pr.info("{} is not mounted!".format(share))
+                pr.info(f"{share} is not mounted!")
 
 
 #TODO: Unmount
