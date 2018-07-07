5edb4acd20e81f19541748c699768f7f5cd61cfe
Johan Kämpe <kampe.johan@gmail.com>
2018-07-07 17:50:46 +0200

    goto: use locations stored in json file

--- goto.py
+++ goto.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
-import os, sys
+import os, sys, json
+
+def load(json_file):
+    """ Loads json file with goto location data """
+    with open(json_file, 'r') as jf:
+        data = json.load(jf)
+    return data
 
 class Goto(object):
     """ Stores 'goto-locations' """
     def __init__(self, shortcut):
-        self.shortcut = shortcut
+        self.shortcuts = [shortcut]
         self.destinations = []
 
     def add_destination(self, destination):
@@ -13,9 +19,14 @@ class Goto(object):
         that can be used if primary is missing """
         self.destinations.append(destination)
 
-    def get_shortcut(self):
+    def get_shortcuts(self):
         """ Returns the shortcut of the Goto """
-        return self.shortcut
+        return self.shortcuts
+
+    def add_shortcut(self, shortcut):
+        """ Adds another shortcut that can be used
+        for the location """
+        self.shortcuts.append(shortcut)
 
     def get_path(self):
         """ Returns the first valid destination,
@@ -27,22 +38,27 @@ class Goto(object):
         return None
 
 HOME = os.path.expanduser("~")
-LOCS = {"dl" : [os.path.join(HOME, "downloads"), os.path.join(HOME, "Downloads")],
-        "code" : [os.path.join(HOME, "code")],
-        "film" : [os.path.join(HOME, "smb", "film")],
-        "tv" : [os.path.join(HOME, "smb", "tv")]}
+SCRIPT_DIR = os.path.realpath(__file__)
+JSON_FILE_PATH = os.path.join(os.path.dirname(SCRIPT_DIR), "goto_locs.json")
+LOCS = load(JSON_FILE_PATH)
 
 GOTOS = []
 ARGUMENT = None
 
 for sc in LOCS:
     g = Goto(sc)
-    for d in LOCS[sc]:
+    for d in LOCS[sc]["destinations"]:
         g.add_destination(d)
+    try:
+        for a in LOCS[sc]["alternatives"]:
+            g.add_shortcut(a)
+    except KeyError:
+        pass
     GOTOS.append(g)
 
 if len(sys.argv) > 1:
     ARGUMENT = sys.argv[1]
     for goto in GOTOS:
-        if ARGUMENT == goto.get_shortcut():
-            print(goto.get_path())
+        for sc in goto.get_shortcuts():
+            if ARGUMENT == sc:
+                print(goto.get_path())
