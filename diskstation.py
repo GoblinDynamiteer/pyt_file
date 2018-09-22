0dcad411d53e8f872459d20daae12964bc0ba17f
Johan Kämpe <kampe.johan@gmail.com>
2018-09-22 09:01:01 +0200

    diskstation: code cleanup
    run auto format
    add shebang
    variable renames
    
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- diskstation.py
+++ diskstation.py
@@ -1,83 +1,94 @@
-# -*- coding: utf-8 -*-
-import os, paths, platform
-from config import configuration_manager as cfg
-import subprocess
-from printout import print_class as pr
+#!/usr/bin/env python3.6
 
-pr = pr(os.path.basename(__file__))
-_config = cfg()
+''' Personal script for interacting with NAS '''
+
+import os
+import platform
+import subprocess
+from config import configuration_manager as cfg
+from printout import print_class as pr
+import paths
+
+PRINT = pr(os.path.basename(__file__))
+CONFIG = cfg()
+NAS_IP = "192.168.0.101"
+
 
 def get_mount_points():
-    return ['TV', 'FILM', 'MISC', 'BACKUP', 'DATA', 'DOCEDU', 'AUDIO']
+    return ['TV', 'FILM', 'MISC', 'BACKUP', 'DATA', 'DOCEDU', 'AUDIO', 'Rest']
+
 
 def get_mount_dest():
-    return _config.get_setting("path", "dsmount")
+    return CONFIG.get_setting("path", "dsmount")
+
 
 def get_home():
-    return _config.get_setting("path", "home")
+    return CONFIG.get_setting("path", "home")
+
 
 def _get_credentials_file(share):
     if share == 'Rest':
         return get_home() + ".smbcredentials_drb"
     return get_home() + ".smbcredentials"
 
-def _print_error_invalid_share(string):
-    pr.error(f"invalid share: [{string}]")
 
-#Mount share
+def _print_error_invalid_share(string):
+    PRINT.error(f"invalid share: [{string}]")
+
+
 def mount(ds_share):
     if platform.system() != 'Linux':
-        pr.error("mount: Not on a Linux-system, quitting.")
+        PRINT.error("mount: Not on a Linux-system, quitting.")
         quit()
-    dsip="192.168.0.101"
+
     mount_dest = get_mount_dest()
     ds_shares = get_mount_points()
     if ds_share == "all" or ds_share.upper() in ds_shares:
         for share in ds_shares:
             if share == ds_share.upper() or ds_share == "all":
                 if ismounted(share):
-                    pr.info(f"{share} is mounted at {get_mount_path(share)}")
+                    PRINT.info(
+                        f"{share} is mounted at {get_mount_path(share)}")
                     continue
                 cred_file = _get_credentials_file(share)
                 opt = f"credentials={cred_file},iocharset=utf8,vers=3.0,rw,file_mode=0777,dir_mode=0777"
-                src = f"//{dsip}/{share}"
+                src = f"//{NAS_IP}/{share}"
                 local_dest = f"{mount_dest}{share.lower()}"
-                command = f"sudo mount -t cifs {src} {local_dest} -o {opt}"
-                pr.info(f"mounting {share} to {local_dest}")
-                #os.system(command)
-                subprocess.call(["sudo", "mount", "-t", "cifs", src, local_dest, "-o", opt])
+                PRINT.info(f"mounting {share} to {local_dest}")
+                subprocess.call(["sudo", "mount", "-t", "cifs",
+                                 src, local_dest, "-o", opt])
     else:
-        _print_error_invalid_share(ds_share);
+        _print_error_invalid_share(ds_share)
 
-# Check if share is already mounted
+
 def ismounted(ds_share):
     if ds_share.lower() == 'rest':
         return
     ds_shares = get_mount_points()
     mount_dest = get_mount_dest()
-    if ds_share.upper() in ds_shares :
+    if ds_share.upper() in ds_shares:
         local_dest = os.path.join(mount_dest, ds_share.lower())
         subdirs = os.listdir(local_dest)
         if len(subdirs) > 1:
             return True
         return False
     else:
-        _print_error_invalid_share(ds_share);
+        _print_error_invalid_share(ds_share)
 
-# Get local mount path of ds share
+
 def get_mount_path(ds_share):
     if ds_share.lower() == 'rest':
         return
     ds_shares = get_mount_points()
     mount_dest = get_mount_dest()
-    if ds_share.upper() in ds_shares :
+    if ds_share.upper() in ds_shares:
         local_dest = os.path.join(mount_dest, ds_share.lower())
         return local_dest
     else:
-        _print_error_invalid_share(ds_share);
+        _print_error_invalid_share(ds_share)
         return None
 
-# Print to console ifmounted
+
 def print_ifmounted(ds_share):
     ds_shares = get_mount_points()
     for share in ds_shares:
@@ -85,10 +96,6 @@ def print_ifmounted(ds_share):
             continue
         if ds_share == "all" or ds_share.lower() == share:
             if ismounted(share):
-                pr.info(f"{share} is mounted at {get_mount_path(share)}")
+                PRINT.info(f"{share} is mounted at {get_mount_path(share)}")
             else:
-                pr.info(f"{share} is not mounted!")
-
-
-#TODO: Unmount
-#sudo umount ~/smb/tv
+                PRINT.info(f"{share} is not mounted!")
